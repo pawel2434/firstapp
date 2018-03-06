abc7f61daeae3424ab7fc7fe904dca3669d7135a
Paweł Owsiany <pawel_24_34@o2.pl>
2018-03-06 19:21:20 +0100

    Kolejne zmiany

--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            //pętla for wykonuje to co w klamrach od 0-9 (10x), krok ++ to co 1
             for (var i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hello World!");
             }
+
+            //blokuje ekran do czasu az wciśniesz guzik
+            Console.ReadKey();
         }
     }
 }
