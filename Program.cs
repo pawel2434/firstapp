3343e1cacbf5cd9ca81b9d044005a0649660e896
Paweł Owsiany <pawel_24_34@o2.pl>
2018-03-06 19:05:43 +0100

    Utworzono projekt

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
